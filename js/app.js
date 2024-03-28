f25de760305659cc9e49160a36ba069e495c61e3
tinuola <24995224+tinuola@users.noreply.github.com>
2024-03-27 23:09:49 -0700

    feat: basic markup with dynamic display of data

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 console.log('Vue!')
+import data from '../data/data.json' with { type: 'json' }
+
+const NotificationsPage = {
+  data() {
+    return {
+      notifications: data,
+    }
+  },
+}
+
+Vue.createApp(NotificationsPage).mount('#app')
